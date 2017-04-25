18810f992677614a1405e4902ccc4de8bea0370f
Skuth <the.skuth@gmail.com>
2017-04-25 16:59:38 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+$app->config('debug', true);
+$app->get('/', function(){
+  $sql = new Skuth\DB\Sql();
+});
+$app->run();
+
+?>
