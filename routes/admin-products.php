f09a3a7ce25108840a0a38a13332049e6c6de25e
Skuth <the.skuth@gmail.com>
2017-08-28 15:44:37 -0300

    Admin - Produtos

--- routes/admin-products.php
+++ routes/admin-products.php
@@ -34,4 +34,41 @@ $app->post('/admin/products/create', function() {
 	exit;
 });
 
+$app->get('/admin/products/:idproduct', function($idproduct) {
+	User::verifyLogin();
+
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$page = new PageAdmin();
+
+	$page->setTpl("products-update", [
+		"product"=>$product->getValues()
+	]);
+});
+
+$app->get('/admin/products/:idproduct/delete', function($idproduct) {
+	User::verifyLogin();
+
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$product->delete();
+
+	header("Location: /admin/products");
+	exit;
+});
+
+$app->post('/admin/products/:idproduct', function($idproduct) {
+	User::verifyLogin();
+
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$product->setData($_POST);
+	$product->save();
+
+	$product->setPhoto($_FILES['file']);
+
+	header("Location: /admin/products");
+	exit;
+});
+
 ?>
